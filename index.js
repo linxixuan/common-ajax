512e65c1a75d41f84ad880a286eabbfcbe312074
morgan <morgan@morgandeMacBook-Pro.local>
2015-06-11 19:56:31 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+var cajax = function (url,callback,postData) {
+    var req = createXMLHTTPObject();
+    if (!req) return;
+    var method = (postData) ? "POST" : "GET";
+    req.open(method,url,true);
+    req.setRequestHeader('User-Agent','XMLHTTP/1.0');
+    if (postData)
+        req.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    req.onreadystatechange = function () {
+        if (req.readyState != 4) return;
+        if (req.status != 200 && req.status != 304) {
+            return;
+        }
+        callback(req);
+    }
+    if (req.readyState == 4) return;
+    req.send(postData);
+}
+
+
+function createXMLHTTPObject() {
+    var xmlhttp = false;
+    var XMLHttpFactories = [
+        function () {return new XMLHttpRequest()},
+        function () {return new ActiveXObject("Msxml2.XMLHTTP")},
+        function () {return new ActiveXObject("Msxml3.XMLHTTP")},
+        function () {return new ActiveXObject("Microsoft.XMLHTTP")}
+    ];
+    for (var i=0;i<XMLHttpFactories.length;i++) {
+        try {
+            xmlhttp = XMLHttpFactories[i]();
+        }
+        catch (e) {
+            continue;
+        }
+        break;
+    }
+    return xmlhttp;
+}
+
+module.exports = cajax;
